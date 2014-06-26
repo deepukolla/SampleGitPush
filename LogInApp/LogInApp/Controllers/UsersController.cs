5300a76e167bdf85e20bdeb4477c9d0015e6ebfe
Pradeep Kolla <pradeepkolla18@hotmail.com>
2014-06-26 11:33:23 -0700

    Files Added..
    Files Added..

---
+++ LogInApp/LogInApp/Controllers/UsersController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Data.Entity;
+
+namespace LogInApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        //
+        // GET: /Users/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(LogInApp.Models.UserLogin user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (IsValid(user.UserName, user.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(user.UserName, false);
+                    return RedirectToAction("Index","Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Login data is incorrect");
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(LogInApp.Models.UserDetail user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (var db = new LogInApp.Models.UserEntities())
+                    {
+                        var crypto = new SimpleCrypto.PBKDF2();
+                        var encrppwd = crypto.Compute(user.Password);
+                        var sysuser = db.UserDetails.Create();
+
+                        //Random r=new Random();
+                        sysuser.Id = user.Id;
+                        sysuser.UserName = user.UserName;
+                        sysuser.Password = encrppwd;
+                        sysuser.EmailId = crypto.Salt;
+                        sysuser.PhoneNo = user.PhoneNo;
+
+                        //TryUpdateModel(sysuser);
+                        db.UserDetails.Add(sysuser);
+                        db.SaveChanges();
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            catch (Exception ex)
+            { 
+            }
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            return View();
+        }
+
+        private bool IsValid(string uname, string pwd)
+        {
+            var crypto = new SimpleCrypto.PBKDF2();
+            bool isvalid = false;
+            using (var db = new LogInApp.Models.UserEntities())
+            {
+                var user = db.UserDetails.FirstOrDefault(u => u.UserName == uname);
+                if (user != null)
+                {
+                    if (user.Password == crypto.Compute(pwd, user.EmailId))
+                    {
+                        isvalid = true;
+                    }
+                }
+            }
+
+            return isvalid;
+        }
+    }
+}
---
+++ LogInApp/LogInApp/Models/UserLogin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LogInApp.Models
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "Please provide UserName", AllowEmptyStrings = false)]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please provide Password", AllowEmptyStrings = false)]
+        [DataType(DataType.Password)]        
+        public string Password { get; set; }  
+    }
+}
